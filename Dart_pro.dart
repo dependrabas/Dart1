157bbf5775b18722d894abacc22715dc3cb3879b
Dependra Basnet <126497750+dependrabas@users.noreply.github.com>
2023-09-10 00:53:05 +0530

    Create Dart_pro.dart

---
+++ Dart_pro.dart
@@ -0,0 +1,10 @@
+void main(){
+  var myList = ['a','b','c','d'];
+  for(var madeUpname in myList){
+    print(madeUpname);
+  }
+  var mySet = {0,1000,1};
+  for(final item in mySet){
+    print(item);
+  }
+}
